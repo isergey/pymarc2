03d95b26563f6a34e922cd8c55f1306ff7bc1d47
Sergey <dostovalov@gmail.com>
2012-01-18 20:55:11 +0400

    firs

---
+++ field.py
@@ -0,0 +1,130 @@
+# encoding: utf-8
+from constants import SUBFIELD_INDICATOR, END_OF_FIELD
+
+
+class Subfield(object):
+    def __init__(self, code, data):
+        self.code = unicode(code)
+        self.data = unicode(data)
+
+    def as_marc(self, to_encoding='utf-8'):
+        """
+        used during conversion of a field to raw marc
+        """
+        return SUBFIELD_INDICATOR + str(self.code) + self.data.encode(to_encoding)
+
+    def __unicode__(self):
+        return u'$' + self.code + self.data
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+
+class LinkedSubfield(object):
+    def __init__(self, code, field=None):
+        self.code = unicode(code)
+        self.field = field
+
+    def as_marc(self, to_encoding='utf-8'):
+        """
+        used during conversion of a field to raw marc
+        """
+        # cut last END_OF_FIELD byte
+        if isinstance(self.field, ControlField):
+            return SUBFIELD_INDICATOR + str(self.code) + self.field.as_marc()[0:-1]
+        else:
+            return SUBFIELD_INDICATOR + str(self.code) +str(self.field.tag)+ self.field.as_marc()[0:-1]
+
+    def __unicode__(self):
+        return u'$' + self.code + u' ' + unicode(self.field)
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+
+class Field(object):
+    def __init__(self, tag):
+        self.tag = unicode(tag)
+
+
+
+
+class ControlField(Field):
+    def __init__(self, tag, data):
+        super(ControlField, self).__init__(tag)
+        self.data = unicode(data)
+
+    def as_marc(self, to_encoding='utf-8'):
+        """
+        used during conversion of a field to raw marc
+        """
+        return self.data.encode(to_encoding) + END_OF_FIELD
+
+    def __unicode__(self):
+        return u'%s %s' % (self.tag, self.data)
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+
+
+
+class DataField(Field):
+    def __init__(self, tag, subfields=[], ind1=u' ', ind2=u' '):
+        super(DataField, self).__init__(tag)
+        self.ind1 = unicode(ind1)
+        self.ind2 = unicode(ind2)
+        self.subfields = {}
+
+        for subfield in subfields:
+            if subfield.code not in self.subfields:
+                self.subfields[subfield.code] = []
+            self.subfields[subfield.code].append(subfield)
+
+    def add_subfield(self, subfield):
+        if subfield.code not in self.subfields:
+            self.subfields[subfield.code] = []
+        self.subfields[subfield.code].append(subfield)
+
+    def as_marc(self, to_encoding='utf-8'):
+        """
+        used during conversion of a field to raw marc
+        """
+
+        marc = [str(self.ind1) + str(self.ind2)]
+        for key in sorted(self.subfields.iterkeys()):
+            for subfield in self.subfields[key]:
+                marc.append(subfield.as_marc(to_encoding))
+        marc.append(END_OF_FIELD)
+        return ''.join(marc)
+
+    def __unicode__(self):
+        ind1 = ind2 = None
+        if self.ind1 == u' ':
+            ind1 = u'#'
+        else:
+            ind1 = self.ind1
+
+        if self.ind2 == u' ':
+            ind2 = u'#'
+        else:
+            ind2 = self.ind2
+
+        strings = []
+        for key in  sorted(self.subfields.iterkeys()):
+            for subfield in self.subfields[key]:
+                if isinstance(subfield, LinkedSubfield):
+                    strings.append(u'\n    '+unicode(subfield))
+                else:
+                    strings.append(unicode(subfield))
+
+        return u'%s %s%s %s' % (self.tag, ind1, ind2, u' '.join(strings))
+
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+#class LinkedField(Field):
+#    super(LinkedField, self).__init__(tag)
+
+
---
+++ record.py
@@ -0,0 +1,301 @@
+# encoding: utf-8
+from array import array
+import exc
+from exc import BaseAddressInvalid
+from marc8 import marc8_to_unicode
+#from exceptions import BaseAddressInvalid, RecordLeaderInvalid,\
+#    BaseAddressNotFound, RecordDirectoryInvalid, NoFieldsFound,\
+#    FieldNotFound
+
+from field import ControlField, DataField, Subfield, LinkedSubfield
+
+from constants import LEADER_LEN, DIRECTORY_ENTRY_LEN, SUBFIELD_INDICATOR, END_OF_FIELD, END_OF_RECORD
+
+class Record(object):
+    def __init__(self, raw='', raw_encoding='utf-8'):
+
+        self.leader = array('c', '          22        4500')
+        self.fields = {}
+        self.raw = raw
+        self.raw_encoding = raw_encoding
+
+#        if len(raw):
+#            self.decode(raw, raw_encoding)
+
+    def _load(self):
+        if self.raw:
+            self.decode(self.raw, self.raw_encoding)
+            self.raw=None
+
+    def decode(self, raw, raw_encoding):
+        """
+        decode_marc() accepts a MARC record in transmission format as a
+        a string argument, and will populate the object based on the data
+        found. The Record constructor actually uses decode_marc() behind
+        the scenes when you pass in a chunk of MARC data to it.
+
+        """
+        # extract record leader
+        self.leader = array('c', raw[0:LEADER_LEN])
+        if len(self.leader) != LEADER_LEN:
+            raise RecordLeaderInvalid
+
+        # extract the byte offset where the record data starts
+        base_address = int(raw[12:17])
+        if base_address <= 0:
+            raise BaseAddressNotFound
+        if base_address >= len(raw):
+            raise BaseAddressInvalid
+
+        # extract directory, base_address-1 is used since the
+        # director ends with an END_OF_FIELD byte
+        directory = raw[LEADER_LEN:base_address - 1]
+
+        # determine the number of fields in record
+        if len(directory) % DIRECTORY_ENTRY_LEN != 0:
+            raise RecordDirectoryInvalid
+        field_total = len(directory) / DIRECTORY_ENTRY_LEN
+
+        # add fields to our record using directory offsets
+        field_count = 0
+        while field_count < field_total:
+            entry_start = field_count * DIRECTORY_ENTRY_LEN
+            entry_end = entry_start + DIRECTORY_ENTRY_LEN
+            entry = directory[entry_start:entry_end]
+            entry_tag = entry[0:3]
+            entry_length = int(entry[3:7])
+            entry_offset = int(entry[7:12])
+            entry_data = raw[base_address + entry_offset:
+            base_address + entry_offset + entry_length - 1]
+
+            # assume controlfields are numeric; replicates ruby-marc behavior
+            if entry_tag < '010' and entry_tag.isdigit():
+                if raw_encoding == 'marc8':
+                    data = marc8_to_unicode(entry_data)
+                else:
+                    data = entry_data.decode(raw_encoding)
+                field = ControlField(tag=entry_tag, data=data)
+            else:
+                subfields = []
+                subs = entry_data.split(SUBFIELD_INDICATOR)
+                ind1 = subs[0][0]
+                ind2 = subs[0][1]
+                for subfield in subs[1:]:
+                    if len(subfield) == 0:
+                        continue
+                    code = subfield[0]
+                    data = subfield[1:]
+
+                    if raw_encoding == 'marc8':
+                        data = marc8_to_unicode(data)
+                    else:
+                        data = data.decode(raw_encoding)
+
+                    subfields.append(Subfield(code=code, data=data))
+
+                field = DataField(
+                    tag=entry_tag,
+                    ind1=ind1,
+                    ind2=ind2,
+                    subfields=subfields,
+                )
+            if field.tag not in self.fields:
+                self.fields[field.tag] = []
+
+            self.fields[field.tag].append(field)
+            field_count += 1
+
+        if field_count == 0:
+            raise NoFieldsFound
+
+    def as_marc(self, to_encoding='utf-8'):
+        """
+        returns the record serialized as MARC21
+        """
+        self._load()
+
+        fields = []
+        directory = []
+        offset = 0
+        to_encoding = to_encoding.lower()
+        if to_encoding == 'utf-8' or to_encoding == 'utf8':
+            self.leader[9] = 'a'
+        # build the directory
+        # each element of the directory includes the tag, the byte length of
+        # the field and the offset from the base address where the field data
+        # can be found
+        for key in sorted(self.fields.iterkeys()):
+
+            for field in self.fields[key]:
+                field_data = field.as_marc(to_encoding)
+                fields.append(field_data)
+                directory.append('%03d' % int(field.tag))
+                directory.append('%04d%05d' % (len(field_data), offset))
+                offset += len(field_data)
+
+
+        # directory ends with an end of field
+        directory.append(END_OF_FIELD)
+
+        # field data ends with an end of record
+        fields.append(END_OF_RECORD)
+
+        # the base address where the directory ends and the field data begins
+        directory = ''.join(directory)
+        base_address = LEADER_LEN + len(directory)
+
+        # figure out the length of the record
+        fields = ''.join(fields)
+        record_length = base_address + len(fields)
+
+        # update the leader with the current record length and base address
+        # the lengths are fixed width and zero padded
+        self.leader = array('c','%05d%s%05d%s' %\
+                      (record_length, self.leader[5:12].tostring(), base_address, self.leader[17:].tostring()))
+
+        # return the encoded record
+        return self.leader.tostring() + directory + fields
+
+    def __unicode__(self):
+        self._load()
+        lines = [self.leader.tostring().replace(' ', '#')]
+        for key in sorted(self.fields.iterkeys()):
+            for field in self.fields[key]:
+                lines.append(unicode(field))
+        return u'\n'.join(lines)
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+
+
+class UnimarcRecord(Record):
+    def __init__(self, raw='', raw_encoding='utf-8'):
+        super(UnimarcRecord,self).__init__(raw, raw_encoding)
+
+    def __unicode__(self):
+        self._load()
+        lines = [self.leader.tostring().replace(' ', '#')]
+        for key in sorted(self.fields.iterkeys()):
+            for field in self.fields[key]:
+                lines.append(unicode(field))
+        return u'\n'.join(lines)
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def decode(self, raw, raw_encoding):
+        """
+        decode_marc() accepts a MARC record in transmission format as a
+        a string argument, and will populate the object based on the data
+        found. The Record constructor actually uses decode_marc() behind
+        the scenes when you pass in a chunk of MARC data to it.
+
+        """
+        # extract record leader
+        self.leader = array('c', raw[0:LEADER_LEN])
+        if len(self.leader) != LEADER_LEN:
+            raise RecordLeaderInvalid
+
+        # extract the byte offset where the record data starts
+        base_address = int(raw[12:17])
+        if base_address <= 0:
+            raise BaseAddressNotFound
+        if base_address >= len(raw):
+            raise BaseAddressInvalid
+
+        # extract directory, base_address-1 is used since the
+        # director ends with an END_OF_FIELD byte
+        directory = raw[LEADER_LEN:base_address - 1]
+
+        # determine the number of fields in record
+        if len(directory) % DIRECTORY_ENTRY_LEN != 0:
+            raise RecordDirectoryInvalid
+        field_total = len(directory) / DIRECTORY_ENTRY_LEN
+
+        # add fields to our record using directory offsets
+        field_count = 0
+        while field_count < field_total:
+            entry_start = field_count * DIRECTORY_ENTRY_LEN
+            entry_end = entry_start + DIRECTORY_ENTRY_LEN
+            entry = directory[entry_start:entry_end]
+            entry_tag = entry[0:3]
+            entry_length = int(entry[3:7])
+            entry_offset = int(entry[7:12])
+            entry_data = raw[base_address + entry_offset:
+            base_address + entry_offset + entry_length - 1]
+
+            # assume controlfields are numeric; replicates ruby-marc behavior
+            if entry_tag < '010' and entry_tag.isdigit():
+                if raw_encoding == 'marc8':
+                    data = marc8_to_unicode(entry_data)
+                else:
+                    data = entry_data.decode(raw_encoding)
+                field = ControlField(tag=entry_tag, data=data)
+            else:
+
+
+
+                subfields = []
+                subs = entry_data.split(SUBFIELD_INDICATOR)
+                ind1 = subs[0][0]
+                ind2 = subs[0][1]
+#########################################################################################
+                #if entry_tag > '399' and entry_tag < '500':
+                if entry_tag[0] == '4':
+                    linked_subfield = None
+                    for subfield in subs[1:]:
+                        code = subfield[0]
+                        data = subfield[1:]
+                        if code == '1':
+                            if linked_subfield:
+                                subfields.append(linked_subfield)
+
+                            linked_subfield = LinkedSubfield(code=code)
+                            linked_field_tag = data[0:3]
+                            if linked_field_tag < '010':
+                                if raw_encoding == 'marc8':
+                                    data = marc8_to_unicode(data)
+                                else:
+                                    data = data.decode(raw_encoding)
+                                linked_subfield.field = ControlField(linked_field_tag, data.decode(raw_encoding))
+                            else:
+                                linked_subfield.field = DataField(tag=linked_field_tag,ind1=data[3], ind2=data[4])
+
+                        else:
+                            if raw_encoding == 'marc8':
+                                data = marc8_to_unicode(data)
+                            else:
+                                data = data.decode(raw_encoding)
+                            linked_subfield.field.add_subfield(Subfield(code, data))
+                    subfields.append(linked_subfield)
+#########################################################################################
+                else:
+                    for subfield in subs[1:]:
+                        if len(subfield) == 0:
+                            continue
+                        code = subfield[0]
+                        data = subfield[1:]
+
+
+                        if raw_encoding == 'marc8':
+                            data = marc8_to_unicode(data)
+                        else:
+                            data = data.decode(raw_encoding)
+
+                        subfields.append(Subfield(code=code, data=data))
+
+                field = DataField(
+                    tag=entry_tag,
+                    ind1=ind1,
+                    ind2=ind2,
+                    subfields=subfields,
+                )
+            if field.tag not in self.fields:
+                self.fields[field.tag] = []
+
+            self.fields[field.tag].append(field)
+            field_count += 1
+
+        if field_count == 0:
+            raise NoFieldsFound
